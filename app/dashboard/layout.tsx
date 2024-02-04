d5fd6246874e2dd635961770826a984fece23dbf
GaspardPierre <pierredillard@gmail.com>
2024-02-04 16:54:14 +0100

    txt freelan

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Invoices | Acme Dashboard',
+  title: 'Invoices | Freemium Dashboard',
 };
  
 export default function Layout({ children }: { children: React.ReactNode }) {
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/font';
 
-export default function AcmeLogo() {
+export default function FreemiumLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-col items-center justify-center leading-10  text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className=" h-6 w-6 md:h-12 md:w-12 rotate-[15deg]" />
+      <p className=" text-[36px] md:text-[44px] ">Freelan</p>
     </div>
   );
 }
